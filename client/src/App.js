54d3f16ad2c007129191d1f3fbc059c430059890
BRASS <mr.brn.lewis@outlook.com>
2021-08-14 21:42:22 +0800

    react-routes for home register login and semantic imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+import 'semantic-ui-css/semantic.min.css';
+import './App.css';
+
+
+import Home from './pages/home'
+import Login from './pages/login'
+import Register from './pages/register'
 
 function App() {
   return (
-      <div> <h1> Welcome to Project CRST </h1></div>
+      <Router>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/login" component={Login}/>
+        <Route exact path="/register" component={Register}/>
+      </Router>
   );
 }
 
