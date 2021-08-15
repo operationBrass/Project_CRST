62ee734aaad088d3341af075ee2f310d4b5e1326
BRASS <mr.brn.lewis@outlook.com>
2021-08-15 19:43:05 +0800

    code mirror implementation on homescreen

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -3,9 +3,14 @@ import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import MainMenu from '../components/menu'
 import { Rail, Form, Grid, Header, TextArea, Message, Segment } from 'semantic-ui-react'
+import CodeMirror from '@uiw/react-codemirror';
+import 'codemirror/keymap/sublime';
+import 'codemirror/theme/monokai.css';
 
 
 function Home() {
+    const code = 'const aRcher = 0;';
+
     const { loading, data,error }  = useQuery(FETCH_NOTES_QUERY);
     if (loading) return 'Loading...';
     if (error) throw new Error("error while retrieving comments");
@@ -13,22 +18,26 @@ function Home() {
        
         <Grid centered columns={2}>
         <Segment>
-                
                 <Rail position="left"> <MainMenu> </MainMenu> </Rail>
    
                 <Grid.Column>
                 <Segment>
-                <Header as='h2' attached='top'>
-      
+    <Header as='h2' attached='top'>
+    <Message>{data.getNotes[0].username} </Message>
     </Header>
     <Segment attached>
-  <p>by Brendan Lewis.. </p>
     </Segment>
                 </Segment>
-                <Form>
-                        <TextArea placeholder='Your code here' style={{ minHeight: 100, minWidth: 300}} />
-                </Form>
-               
+                
+                    <CodeMirror
+                    value={code}
+                    options={{
+                        theme: 'monokai',
+                        keyMap: 'sublime',
+                        mode: 'jsx',
+                    }}
+                    />
+                    
                 <Segment>
                         <Header as="h3">Comments</Header>
                     </Segment>
