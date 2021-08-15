d096eb772d66a71ec72bad06e1a0961f3fea1775
BRASS <mr.brn.lewis@outlook.com>
2021-08-15 16:11:09 +0800

    basic setup of Home page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,13 +2,34 @@ import React from 'react'
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import MainMenu from '../components/menu'
+import { Rail, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
 
 function Home() {
     const { loading, data } = useQuery(FETCH_NOTES_QUERY)
     console.log(JSON.stringify(data)) //checking result of the query
-    return(
-        <div><MainMenu> </MainMenu> <h1>Home</h1> </div>
+    return (
+       
+        <Grid centered columns={3}>
+        <Segment>
+                
+                <Rail position="left"> <MainMenu> </MainMenu> </Rail>
+   
+                <Grid.Column>
+                <Segment>
+                        <Header as="h2"> Code Snippet </Header>
+                </Segment>
+                        <textarea></textarea>
+               
+                <Segment>
+                    <Header as="h3">Notes</Header>
+                </Segment>
+                </Grid.Column>
+                <Rail position="right">   <h6> Logout </h6> </Rail>
+            
+        </Segment>
+        </Grid>
+       
     )
 }
 
